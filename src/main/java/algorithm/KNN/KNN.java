803ca3def098e07fa3b83c3ee4eff0c772a2df67
ytchen <120903400@qq.com>
2016-11-22 19:25:39 +0800

    Create
    Create

---
+++ src/main/java/algorithm/KNN/KNN.java
@@ -0,0 +1,17 @@
+package algorithm.KNN;
+
+/**
+ * @ClassName:
+ * @Description: TODO
+ * @Function List: TODO
+ * @author: ytchen
+ * @version:
+ * @Date: 2016/11/22
+ * @History: //历史修改记录
+ * <author>  // 修改人
+ * <time> //修改时间
+ * <version> //版本
+ * <desc> // 描述修改内容
+ */
+public class KNN {
+}
